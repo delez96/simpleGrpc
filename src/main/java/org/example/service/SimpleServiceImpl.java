75d009aaeca6b81df3546e2fe4ff77ed6805dc50
delez <kvakvakevkav@gmail.com>
2024-06-09 18:34:35 +0300

    first commit

---
+++ src/main/java/org/example/service/SimpleServiceImpl.java
@@ -0,0 +1,24 @@
+package org.example.service;
+
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+import org.example.grpc.HelloRequest;
+import org.example.grpc.HelloResponse;
+import org.example.grpc.HelloServiceGrpc;
+
+@GrpcService
+public class SimpleServiceImpl extends HelloServiceGrpc.HelloServiceImplBase {
+
+    @Override
+    public void hello(HelloRequest request, StreamObserver<HelloResponse> responseObserver) {
+        System.out.println(request);
+
+        HelloResponse response = HelloResponse.newBuilder()
+                .setGreeting("Hello, " + request.getFirstName() + " " + request.getLastName())
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+}
